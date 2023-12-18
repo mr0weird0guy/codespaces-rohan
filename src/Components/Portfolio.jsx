90b463d2d623d682a7608166c7fa79fa60919273
Rohan <rohandiwan1824@gmail.com>
2023-12-18 09:57:09 +0000

    corrections

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -54,7 +54,7 @@ const projectList = [
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/mr0weird0guy/MIAPlumbingSolutions",
+    url: "https://github.com/mr0weird0guy/codespaces-rohan",
   },
   {
     title: "News and Articles website",
